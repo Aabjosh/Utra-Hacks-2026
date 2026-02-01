532b54848c1fda82fd322338bf3dc09fb92541db
Aabjosh_Singh <aabjosh0singh@gmail.com>
2026-01-31 20:06:46 -0500

    added sample processing images

--- pathSegmenter.py
+++ pathSegmenter.py
@@ -1,12 +1,18 @@
 from google import genai
 
 # init constants
-# TAPE_WIDTH = 2 # inches
+TAPE_WIDTH = 2 # inches
+FILE_NAME = "message.txt"
+LENGTH_KEY = "$LENGTH$"
+WIDTH_KEY = "$WIDTH$"
+TAPE_THICKNESS_KEY = "$THICKNESS$"
+
+# add locations for images (add to repo)
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
 response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="who is aabjosh singh from uwaterloo mechatronics eng"
+    model="gemini-3-flash-preview", contents=""
 )
 print(response.text)
