18d700bbeaad92504df2e160cb896b26ebd7faf8
Aabjosh_Singh <aabjosh0singh@gmail.com>
2026-02-01 01:30:56 -0500

    random additions

--- gui/pathSegmenter.py
+++ gui/pathSegmenter.py
@@ -3,22 +3,25 @@ from google.genai import types
 from PIL import Image
 
 # init constants
-TAPE_WIDTH = 2 # inches
+TAPE_WIDTH = 2.0 # inches
+MINIMUM_LENGTH = 220.0
+MAXIMUM_LENGTH = 237.0
 FILE_NAME = "messagebank/message.txt"
 SAMPLE_FILE = "messagebank/testMessage.txt"
 
 TAPE_THICKNESS_KEY = "$THICKNESS$"
-LENGTH_KEY = "$LENGTH$"
-WIDTH_KEY = "$WIDTH$"
+MAX_LENGTH_KEY = "$MAX_LENGTH$"
+MIN_LENGTH_KEY = "$MIN_LENGTH$"
 
 SAMPLE_IMAGE_1_DIRECT = "images/SAMPLE_223_1.png"
 SAMPLE_IMAGE_2_DIRECT = "images/SAMPLE_223_2.png"
+ATTEMPT_AT_CALCULATING_DIRECT = "images/unnamed (3).jpg"
 
-def fileReplacer( fileAsString , segmentThickness , boxLength , boxWidth ):
-    output = fileAsString.replace( TAPE_THICKNESS_KEY , str(segmentThickness) )
-    output = output.replace( LENGTH_KEY , str(boxLength) )
-    output = output.replace( WIDTH_KEY , str(boxWidth) )
-    return output
+# def fileReplacer( fileAsString , segmentThickness , boxLength , boxWidth ):
+#     output = fileAsString.replace( TAPE_THICKNESS_KEY , str(segmentThickness) )
+#     output = output.replace( LENGTH_KEY , str(boxLength) )
+#     output = output.replace( WIDTH_KEY , str(boxWidth) )
+#     return output
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
@@ -26,13 +29,16 @@ client = genai.Client()
 with open ( FILE_NAME , "r" ) as file:
     fileContents = file.read()
 
-messageToGemini = fileReplacer( fileContents , 2 , 10 , 20 )
+# messageToGemini = fileReplacer( fileContents , 2 , 10 , 20 )
 
 with open( SAMPLE_IMAGE_1_DIRECT , "rb" ) as f:
     imageBytes1 = f.read()
 
 with open( SAMPLE_IMAGE_2_DIRECT , "rb" ) as f:
     imageBytes2 = f.read()
+
+with open( ATTEMPT_AT_CALCULATING_DIRECT , "rb" ) as f:
+    imageBytesData = f.read()
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
@@ -50,6 +56,10 @@ response = client.models.generate_content(
                 types.Part.from_bytes(
                     data = imageBytes2,
                     mime_type = "image/png"
+                ),
+                types.Part.from_bytes(
+                    data = imageBytesData,
+                    mime_type = "image/jpeg"
                 )
             ]
         )
